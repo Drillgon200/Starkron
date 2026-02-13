24a184f22e466503452f11d910cb8278ed9f72cd
ROSS O. <157574003+ROSSumOwen453@users.noreply.github.com>
2026-02-13 12:57:49 -0800

    demo block out
    demo block out

--- Assets/Scripts/MenuV1.cs
+++ Assets/Scripts/MenuV1.cs
@@ -25,7 +25,7 @@ public class MenuV1 : MonoBehaviour
 
     public void ExitPrompt()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (exitOverlayGraphic.activeSelf == false && mainMenuScene.activeSelf == false)
             {
